deacefdf6306a2b0558120a12b78f603fa3846f8
samadon1 <calvinsamueldonkor@gmail.com>
2022-08-17 16:21:04 +0000

    triple upfate

--- Two pointers/triplets_sum.py
+++ Two pointers/triplets_sum.py
@@ -15,20 +15,29 @@ Approach:
 """
 
 def triplet_sum(array):
-    array = sorted(array)                               #Sort the array
-    for i in range(len(array)):                         #For each element in the array
+    array = sorted(array)      
+    res = []                         #Sort the array
+    for i in range(len(array)):
+        if i > 0 and array[i] == array[i-1]:
+            continue                         #For each element in the array
         
         l, r = i+1, len(array) - 1                      #create a right pointer and a left pointer for the other pairs
         target = -array[i]                              # target should be the neg of the first element
         while l < r:
             sum = array[l] + array[r]   
             if sum == target:                           #If they are the same
-                return array[i], array[r], array[l]     #return the triples
+                res.append([array[i], array[r], array[l]])     #return the triples
+                l +=1
+                r -=1
+                while l < r and array[l] == array[l-1]:
+                    l += 1
+                while l < r and array[r] == array[r+1]:
+                    r -= 1
             elif(sum > target):                     
                 r -= 1                                  #if the sum is greater than target decrement right pointer 
             else:
                 l += 1                                  #if the sum is less than target increment left pointer
-    return 0
+    return res
 
 array = [-3, 5, 1,0,-2 , 2]
 print(triplet_sum(array))
